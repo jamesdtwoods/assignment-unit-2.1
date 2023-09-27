af366cbd2847ba344b3ea390ab7050028de9e12c
James Woods <james.d.t.woods@gmail.com>
2023-09-26 20:29:28 -0500

    finished stretch scripts

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -91,7 +91,13 @@ console.log(petStatus);
 //      assigns the highest value to `mostPets`. There's several possibilities --
 //      be sure to think through all the scenarios. 
 //      console.log `mostPets` after the conditional has run.
-
+let mostPets = '';
+if ( pets >= friendsPets ){
+  mostPets = pets;
+} else {
+  mostPets = friendsPets;
+};
+console.log(mostPets);
 // 16 - Make a variable called `luckyResult`
 //      Write a *switch* statement that sets `luckyResult` to:
 //      "First is the worst" if your lucky number is 1
@@ -100,10 +106,26 @@ console.log(petStatus);
 //      Otherwise, "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 //      console.log `luckyResult` after the conditional has run.
-
+let luckyResult = '';
+switch (luckyNumber) {
+  case 1:
+    luckyResult = 'First is the worst';
+    break;
+  case 2:
+    luckyResult = 'Second is the best';
+    break;
+  case 3:
+    luckyResult = 'Third is the one with the polka dot dress';
+    break;
+  default:
+    luckyResult = 'Luck is what happens when preparation meets opportunity';
+}
+console.log(luckyResult);
 // 17 -- Rewrite question 12 with a `ternary` operator. You'll need to do research!
-
-
+function getResult(result) {
+  return result ? 'Adventures are great!' : 'How about we stay home?';
+};
+console.log(result);
 
 
 
